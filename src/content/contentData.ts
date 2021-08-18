0b3942cff046b3b6c19219d5b059f8fbb474bde1
Sefidi <me@ghonchesefidi.ir>
2021-08-18 04:30:00 +0430

    Add source fules

---
+++ src/content/contentData.ts
@@ -0,0 +1,167 @@
+const contentData = [
+    {
+        id: 'awdawawdadwawdwaddwawdda',
+        name: {
+            persian: 'تست یک',
+            english: 'test one'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+    {
+        name: {
+            persian: 'تست دو',
+            english: 'test two'
+        },
+        image: 'https://via.placeholder.com/500x500',
+        translation: 'تست قسمت ترجمه دو',
+        src: 'https://nini-songs.s3.ir-thr-at1.arvanstorage.com/test1'
+    },
+]
+export default contentData
+
